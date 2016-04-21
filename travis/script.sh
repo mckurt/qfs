34d81037be8108b1e37a3e93f56b5bc2226090c9
Mehmet Can Kurt <mcankurt@quantcast.com>
2016-04-21 15:36:30 -0700

    test for code coverage

--- travis/script.sh
+++ travis/script.sh
@@ -10,7 +10,7 @@ DEPS_UBUNTU="g++ cmake git libboost-regex-dev libkrb5-dev xfslibs-dev libssl-dev
 DEPS_CENTOS="gcc-c++ make cmake git boost-devel krb5-devel xfsprogs-devel openssl-devel python-devel fuse-devel java-openjdk java-devel libuuid-devel"
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    make gtest tarball
+    CMAKE_OPTIONS="-D ENABLE_COVERAGE=yes" make gtest tarball && gcov -o build/debug/src/cc/tests/CMakeFiles/test.t.dir/ integtest_main.cc.o
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
@@ -21,7 +21,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
         CMD="yum install -y $DEPS_CENTOS"
     fi
 
-    CMD="$CMD && make gtest tarball"
+    CMD="$CMD && CMAKE_OPTIONS=\"-D ENABLE_COVERAGE=yes\" make gtest tarball"
+    # when CMAKE_OPTIONS is provided w/o build type, it defaults to debug.  
+    CMD="$CMD && gcov -o build/debug/src/cc/tests/CMakeFiles/test.t.dir/ integtest_main.cc.o"
     docker run --rm -t -v $PWD:$PWD -w $PWD $DISTRO:$VER /bin/bash -c "$CMD"
 fi
 
